85ae30a344d9a0202a5b2a687f382cf9c2b97d64
chente09 <chentenengerc@gmail.com>
2024-01-21 16:07:19 -0500

    final2

--- php/registro.php
+++ php/registro.php
@@ -1,11 +1,20 @@
 <?php
     include 'conexion.php';
 
-    $nombre=$_POST['nombre'];
-    $apellido=$_POST['apellido'];
+    $nombres=$_POST['nombres'];
+    $apellidos=$_POST['apellidos'];
     $email=$_POST['email'];
+    $celular=$_POST['celular'];
+    $direccion=$_POST['direccion'];
+    $cod_postal=$_POST['cod_postal'];
+    $ciudad=$_POST['ciudad'];
+    $pais=$_POST['pais'];
+    $comentarios=$_POST['comentarios'];
 
-    $consulta="INSERT into usuario(nombre, apellido, email) values('$nombre','$apellido','$email')";
+
+
+    $consulta="INSERT into usuario(nombres, apellidos, email, celular, direccion, cod_postal, ciudad, pais, comentarios)
+     values('$nombres','$apellidos','$email','$celular','$direccion','$cod_postal','$ciudad','$pais','$comentarios')";
 
     $resultado=mysqli_query($conexion,$consulta);
 
